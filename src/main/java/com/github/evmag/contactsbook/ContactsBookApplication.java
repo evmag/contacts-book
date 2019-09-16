20b58b0845d10a32dff9ff61bcca63f7901a43ca
evmag <vag.mg4@gmail.com>
2019-09-16 09:44:18 +0200

    Import project from Spring Inititalizr

---
+++ src/main/java/com/github/evmag/contactsbook/ContactsBookApplication.java
@@ -0,0 +1,13 @@
+package com.github.evmag.contactsbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactsBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactsBookApplication.class, args);
+	}
+
+}
