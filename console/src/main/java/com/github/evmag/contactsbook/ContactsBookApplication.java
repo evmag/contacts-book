52d5745b3577ec6e727a589675b035ca1dbde582
evmag <vag.mg4@gmail.com>
2019-09-17 12:30:51 +0200

    Implement console run.

--- console/src/main/java/com/github/evmag/contactsbook/ContactsBookApplication.java
+++ console/src/main/java/com/github/evmag/contactsbook/ContactsBookApplication.java
@@ -1,13 +1,46 @@
 package com.github.evmag.contactsbook;
 
+import com.github.evmag.contactsbook.model.Contact;
+import com.github.evmag.contactsbook.service.ContactsBookService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ContactsBookApplication {
+import java.util.List;
 
+@SpringBootApplication
+public class ContactsBookApplication implements CommandLineRunner {
+    // === Constants ===
+    private static final Logger log = LoggerFactory.getLogger(ContactsBookApplication.class);
+
+    // === Fields ===
+    private final ContactsBookService contactsBookService;
+
+    // === Main ===
 	public static void main(String[] args) {
+	    log.trace("Starting console application from {} ...", ContactsBookApplication.class.getSimpleName());
 		SpringApplication.run(ContactsBookApplication.class, args);
+        log.trace("Ending console application from {} ...", ContactsBookApplication.class.getSimpleName());
 	}
+
+    // === Constructors ===
+    @Autowired
+    public ContactsBookApplication(ContactsBookService contactsBookService) {
+        this.contactsBookService = contactsBookService;
+    }
+
+    // === Public methods ===
+    @Override
+    public void run(String... args) throws Exception {
+        List<Contact> contacts = contactsBookService.getContacts();
+
+        for (Contact contact : contacts) {
+            System.out.println(contact);
+        }
+
+    }
 
 }
