0af5c442248bb8f2fc63e47cee5ab2fd08d5080f
evmag <vag.mg4@gmail.com>
2019-10-04 18:20:11 +0200

    Add web module.

---
+++ web/src/main/java/com/github/evmag/contactsbook/ContactsBookWebApplication.java
@@ -0,0 +1,11 @@
+package com.github.evmag.contactsbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactsBookWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ContactsBookWebApplication.class, args);
+    }
+}
