4337c7445448587e4fe34b4cee8a4ec7f431cba6
evmag <vag.mg4@gmail.com>
2020-03-31 21:58:04 +0200

    Add database annotations to Contact class.

--- core/src/main/java/com/github/evmag/contactsbook/model/Contact.java
+++ core/src/main/java/com/github/evmag/contactsbook/model/Contact.java
@@ -2,17 +2,37 @@ package com.github.evmag.contactsbook.model;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "contacts")
 public class Contact {
     // === Fields ===
+    @Id
+    @Column(name = "id")
     private int id = -1;
+
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
+
+    @Column(name = "phone")
     private String phone;
+
+    @Column(name = "email")
     private String email;
+
+    @Column(name = "date_of_birth")
     @DateTimeFormat(pattern = "yyyy-MM-dd") // Required for displaying the date with Thymeleaf in HTML date form
     private LocalDate dateOfBirth;
+
+    @Column(name = "notes")
     private String notes;
 
     // === Constructors ===
