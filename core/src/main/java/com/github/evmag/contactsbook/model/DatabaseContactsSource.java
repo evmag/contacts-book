b76960066b0ee7dc16f45c306eeb791bed0bcac0
evmag <vag.mg4@gmail.com>
2020-04-01 18:05:38 +0200

    Implement get all contacts from database datasource.

---
+++ core/src/main/java/com/github/evmag/contactsbook/model/DatabaseContactsSource.java
@@ -0,0 +1,61 @@
+package com.github.evmag.contactsbook.model;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Primary //TODO: move bean selection elsewhere
+@Repository
+public class DatabaseContactsSource implements ContactsSource{
+    // === Constants ===
+    private static final Logger log = LoggerFactory.getLogger(DatabaseContactsSource.class);
+
+    // === Fields ==
+
+    @PersistenceContext
+    private EntityManager entityManger;
+
+    // === Public methods ==
+
+    @Override
+    @Transactional
+    public List<Contact> getAllContacts() {
+        return entityManger.createQuery("select c from Contact c", Contact.class).getResultList();
+    }
+
+    @Override
+    public Contact getContact(int contactId) {
+        return null;
+    }
+
+    @Override
+    public int addContact(Contact contact) {
+        return 0;
+    }
+
+    @Override
+    public boolean removeContact(Contact contact) {
+        return false;
+    }
+
+    @Override
+    public boolean removeContact(int contactId) {
+        return false;
+    }
+
+    @Override
+    public boolean editContact(Contact contactToEdit, Contact contactEdited) {
+        return false;
+    }
+
+    @Override
+    public int getContactsCount() {
+        return 0;
+    }
+}
